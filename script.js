fbba21eb5ccbb5831b0a28e6ebede7778f71de00
avocadic <theneonburrito@gmail.com>
2024-09-13 19:22:33 -0500

    Initial image push

--- script.js
+++ script.js
@@ -23,6 +23,26 @@ document.addEventListener("DOMContentLoaded", function () {
                 dropdown.appendChild(option);
             }
         }
+        // Add an event listener to update the image when the dropdown value changes
+        dropdown.addEventListener('change', function () {
+            updatePokemonImage(dropdown);
+        });
+    }
+    function updatePokemonImage(dropdown) {
+        const selectedKey = dropdown.value;
+        const pokemon = pokemonData[selectedKey];
+
+        if (pokemon) {
+            const baseId = pokemon.base_id.toString().padStart(4, '0');
+            const formId = pokemon.form_id.toString().padStart(2, '0');
+            const imageName = pokemon.form_name ? `${baseId}_${formId}.png` : `${baseId}.png`;
+            const imageUrl = `img/pkmn/${imageName}`;
+
+            // Update the src attribute of the img element
+            const imgElement = document.getElementById('pokemon-image');
+            imgElement.src = imageUrl;
+            imgElement.alt = pokemon.name;
+        }
     }
     // Populate each dropdown
     dropdownIds.forEach(id => populateDropdown(id));
